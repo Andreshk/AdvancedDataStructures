04cf544dff8e9dd79f86a39af098d6d8d3f860a6
Andrey Drenski <andreshko.dre@gmail.com>
2018-01-13 21:53:44 +0200

    small fixes

--- PairingHeap.hs
+++ PairingHeap.hs
@@ -1,10 +1,8 @@
 module PairingHeap
-( insert, merge, (<>), getMin,
+( insert, (<>), merge, getMin,
   extractMin, empty, size,
   fromList, toList, sort
 ) where
-
-import Data.List (foldl')
 
 -- No need for comments - the code is self-documenting.
 -- The basic operations such as insert, merge and findMin have O(1)
@@ -13,16 +11,18 @@ import Data.List (foldl')
 data PairingHeap a = Empty | Node a [PairingHeap a]
 
 instance (Ord a) => Eq (PairingHeap a) where
-    Empty == Empty = True
-    h1 == h2 = (toList h1) == (toList h2)
+    ph1 == ph2 = (toList ph1) == (toList ph2)
 
 instance (Show a) => Show (PairingHeap a) where
     show Empty = "PairingHeap{}"
-    show h = let Just val = getMin h in "PairingHeap{" ++ show val ++ "..}"
-    
+    show (Node val _) = "PairingHeap{" ++ show val ++ "..}"
+
 insert :: Ord a => a -> PairingHeap a -> PairingHeap a
-insert x t = singleton x <> t
+insert x ph = singleton x <> ph
   where singleton x = Node x []
+
+(<>) :: Ord a => PairingHeap a -> PairingHeap a -> PairingHeap a
+(<>) = merge
 
 merge :: Ord a => PairingHeap a -> PairingHeap a -> PairingHeap a
 merge ph1 Empty = ph1
@@ -30,9 +30,6 @@ merge Empty ph2 = ph2
 merge ph1@(Node v1 ch1) ph2@(Node v2 ch2)
     | v1 < v2   = Node v1 (ph2:ch1)
     | otherwise = Node v2 (ph1:ch2)
-
-(<>) :: Ord a => PairingHeap a -> PairingHeap a -> PairingHeap a
-(<>) = merge
 
 getMin :: PairingHeap a -> Maybe a
 getMin Empty = Nothing
@@ -49,18 +46,17 @@ empty :: PairingHeap a -> Bool
 empty Empty = True
 empty _ = False
 
--- unfortunately O(n)
+-- unfortunately O(nlgn)...
 size :: Ord a => PairingHeap a -> Int
 size = length . toList
 
 fromList :: Ord a => [a] -> PairingHeap a
-fromList = foldl' (flip insert) Empty
+fromList = foldl (flip insert) Empty
 -- fromList = foldr insert Empty
 
 toList :: Ord a => PairingHeap a -> [a]
-toList = toList' . extractMin
-  where toList' Nothing = []
-        toList' (Just (val, t)) = val : toList' (extractMin t)
+toList ph = case extractMin ph of Nothing -> []
+                                  Just (val, ph1) -> val : toList ph1
 
 sort :: Ord a => [a] -> [a]
 sort = toList . fromList
