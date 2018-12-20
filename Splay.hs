d547c4efa488cc5dc7a07d658661ae2467d0fa07
Andrey Drenski <andreshko.dre@gmail.com>
2018-12-20 11:32:59 +0200

    add comments

--- Splay.hs
+++ Splay.hs
@@ -7,12 +7,13 @@ data Splay a = Empty | Node a (Splay a) (Splay a) deriving Show
 empty :: Splay a -> Bool
 empty Empty = True
 empty _     = False
-
+-- Standard tree rotations
 rotateLeft, rotateRight :: Splay a -> Splay a
 rotateLeft  (Node x a (Node y b c)) = (Node y (Node x a b) c)
 rotateRight (Node y (Node x a b) c) = (Node x a (Node y b c))
 
 -- Naive implementation, does not splay optimally (!)
+-- Invariant: `insert x _` returns a tree with x in its root
 insert :: Ord a => a -> Splay a -> Splay a
 insert x Empty = Node x Empty Empty
 insert x t@(Node y l r)
@@ -21,6 +22,7 @@ insert x t@(Node y l r)
   | x > y  = rotateLeft  (Node y l (insert x r))
 
 -- Naive implementation, does not splay optimally (!)
+-- Invariant: if lookup x _ returns a tree, its root is x
 lookup :: Ord a => a -> Splay a -> Maybe (Splay a)
 lookup _ Empty = Nothing
 lookup x t@(Node y l r)
