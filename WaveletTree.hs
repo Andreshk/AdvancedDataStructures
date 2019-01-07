e9e51cc9fdf8e8d244e868fee47ba68bb3c45241
Andrey Drenski <andreshko.dre@gmail.com>
2019-01-08 01:25:40 +0200

    add typeclass constraint for wavelet trees & validation

--- WaveletTree.hs
+++ WaveletTree.hs
@@ -1,33 +1,48 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE UndecidableInstances #-}
+
 module WaveletTree where
+import Data.Char (chr,ord)
 import Data.List (partition)
-import Data.BitVector (BitVector, showBin, (!.), most, fromBits) -- requires package bv
+import Data.BitVector (BitVector,showBin,(!.),most,fromBits) -- requires package bv
 import qualified Data.BitVector as BV (foldl)
 
-data WaveletTree a = Leaf a | Node BitVector (WaveletTree a) (WaveletTree a)
+data WaveletTree a = Leaf (Maybe a) | Node BitVector (WaveletTree a) (WaveletTree a)
+
+class Wv a where
+  midpoint :: a -> a -> a
+instance Wv Char where
+  midpoint x y = chr $ (ord x + ord y) `div` 2
+instance {-# OVERLAPPABLE #-} (Enum a, Integral a) => Wv a where
+  midpoint x y = (x + y) `div` 2
 
 instance Show a => Show (WaveletTree a) where
   show w = show' 0 w
-    where show' pad (Leaf x) = replicate pad ' ' ++ show x
+    where show' _ (Leaf Nothing) = ""
+          show' pad (Leaf (Just x)) = replicate pad ' ' ++ show x
           show' pad (Node bitmap left right) = replicate pad ' '
                                             ++ showBin bitmap
                                             ++ "\n" ++ show' (pad+2) left
                                             ++ "\n" ++ show' (pad+2) right
 
 -- It is assumed that (minimum xs) == 1 and (maximum xs) is the size of the alphabet
-wavelet :: Integral a => [a] -> WaveletTree a
+wavelet :: (Wv a, Ord a, Enum a) => [a] -> WaveletTree a
 wavelet xs = wavelet' (minimum xs) (maximum xs) xs
   where wavelet' from to xs
-          | null xs    = Leaf (-1) -- fake node, will never be reached during indexing
-          | from == to = Leaf from
-          | otherwise  = Node bitmap (wavelet' from mid ys) (wavelet' (mid+1) to zs)
-          where mid = (from + to) `div` 2
+          | null xs    = Leaf Nothing -- fake node, will never be reached during indexing
+          | from == to = Leaf $ Just from
+          | otherwise  = Node bitmap (wavelet' from mid ys) (wavelet' (succ mid) to zs)
+          where mid = midpoint from to
                 bitmap = fromBits $ map (>mid) xs
                 (ys,zs) = partition (<=mid) xs
 
 (!) :: Integral ix => WaveletTree a -> ix -> a
-(Leaf x) ! _ = x
+(Leaf (Just x)) ! _ = x
 (Node bitmap left right) ! i
   | bitmap !. i = right ! (rank i)
   | otherwise   = left ! (i - rank i)
   where rank 0 = 0
         rank i = BV.foldl (\c b -> if b then c+1 else c) 0 $ most i bitmap
+
+check :: (Wv a, Ord a, Enum a) => [a] -> Bool
+check xs = xs == map (wavelet xs !) [0..length xs - 1]
