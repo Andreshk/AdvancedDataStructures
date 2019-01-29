bb2142ef291a969171e0fd709397f46e2d7cb38c
Andrey Drenski <andreshko.dre@gmail.com>
2019-01-29 19:56:15 +0200

    add Sequence class & appropriate instances

--- WaveletTree.hs
+++ WaveletTree.hs
@@ -1,10 +1,13 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE UndecidableInstances #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ConstrainedClassMethods #-}
 
 module WaveletTree where
 import Data.Char (chr,ord)
 import Data.List (partition,sort,nub) -- sort,nub needed for testing only
-import Data.BitVector (BitVector,showBin,nil,(!.),most,fromBits) -- requires package bv
+import Data.BitVector (BitVector,BV,showBin,nil,(!.),most,fromBits) -- requires package bv
 import qualified Data.BitVector as BV (foldl)
 
 data Tree a = Dummy | Leaf a | Node BitVector (Tree a) (Tree a)
@@ -30,6 +33,35 @@ instance Show a => Show (WaveletTree a) where
                                             ++ "\n" ++ show' (pad+2) left
                                             ++ "\n" ++ show' (pad+2) right
 
+{- A sequence should support the following three operations:
+    Access: the element at a given position in the sequence
+    Rank: The number of occurences of a given element among the first i in the sequence
+    Select: The position of the i-th occurence of a given element in the sequence -}
+class Sequence c where
+    type ElemType c :: *
+    (!) :: c -> Int -> ElemType c
+    rank, select :: Eq (ElemType c) => ElemType c -> Int -> c -> Int
+
+-- Example implementation of the sequence operations for lists
+instance Sequence [a] where
+  type ElemType [a] = a
+  (!) = (!!)
+  rank c i lst = length . filter (==c) $ take i lst
+  select c i (x:xs)
+    | c == x    = if i == 1 then 0 else 1 + select c (i-1) xs
+    | otherwise = 1 + select c i xs
+
+instance Sequence BV where -- BitVector is actually a synonym
+    type ElemType BV = Bool
+    (!) = (!.)
+    rank False i bv = i - (rank True i bv)
+    rank True 0 _ = 0
+    rank True i bitmap = BV.foldl (\c b -> if b then c+1 else c) 0 $ most i bitmap
+    select b i bv = select' i 0
+      where select' i curr
+              | bv !. curr == b = if i == 1 then curr else select' (i-1) (curr+1)
+              | otherwise       = select' i (curr+1)
+
 wavelet :: Wv a => [a] -> WaveletTree a
 wavelet xs = WaveletTree t (a,b)
   where a = minimum xs
@@ -46,47 +78,31 @@ wavelet xs = WaveletTree t (a,b)
                         left  = wavelet' (goL range) ys
                         right = wavelet' (goR range) zs
 
--- Rank: the number of set bits among the first i bits in the bitvector
-rank :: Integral ix => ix -> BitVector -> ix
-rank 0 bitmap = 0
-rank i bitmap = BV.foldl (\c b -> if b then c+1 else c) 0 $ most i bitmap
+instance Wv a => Sequence (WaveletTree a) where
+  type ElemType (WaveletTree a) = a
+  (WaveletTree t _) ! i = t ! i
+    where (Leaf x) ! _ = x
+          (Node _ left Dummy) ! i = left ! i    {- A dummy node corresponds to an empty subset of -}
+          (Node _ Dummy right) ! i = right ! i  {- characters => go directly to the other branch. -}
+          (Node bitmap left right) ! i
+            | bitmap !. i = right ! (rank True i bitmap)
+            | otherwise   = left ! (rank False i bitmap)
 
--- Select: the position of the ith bit with the corresponding value
-select :: Integral ix => Bool -> ix -> BitVector -> ix
-select b i bv = select' i 0
-  where select' i curr
-          | bv !. curr == b = if i == 1 then curr else select' (i-1) (curr+1)
-          | otherwise       = select' i (curr+1)
+  rank c i (WaveletTree w range) = rank' c i w range
+    where rank' _ i (Leaf _) _ = i -- all unused arguments should contain the same symbol
+          rank' c i (Node _ left Dummy) range  = rank' c i left (goL range)
+          rank' c i (Node _ Dummy right) range = rank' c i right (goR range)
+          rank' c i (Node bitmap left right) range@(a,b)
+            | c <= (midpoint a b) = rank' c (rank False i bitmap) left (goL range)
+            | otherwise           = rank' c (rank True i bitmap) right (goR range)
 
--- The i-th character from the original sequence
-(!) :: Integral ix => WaveletTree a -> ix -> a
-(WaveletTree t _) ! i = t ! i
-  where (Leaf x) ! _ = x
-        (Node _ left Dummy) ! i = left ! i    {- A dummy node corresponds to an empty subset of -}
-        (Node _ Dummy right) ! i = right ! i  {- characters => go directly to the other branch. -}
-        (Node bitmap left right) ! i
-          | bitmap !. i = right ! (rank i bitmap)
-          | otherwise   = left ! (i - rank i bitmap)
-
--- The number of occurences of a given symbol among the first i in the sequence
-wvrank :: (Wv a, Integral ix) => a -> ix -> WaveletTree a -> ix
-wvrank c i (WaveletTree w range) = wvrank' c i w range
-  where wvrank' _ i (Leaf _) _ = i -- all unused arguments should contain the same symbol
-        wvrank' c i (Node _ left Dummy) range  = wvrank' c i left (goL range)
-        wvrank' c i (Node _ Dummy right) range = wvrank' c i right (goR range)
-        wvrank' c i (Node bitmap left right) range@(a,b)
-          | c <= (midpoint a b) = wvrank' c (i - rank i bitmap) left (goL range)
-          | otherwise           = wvrank' c (rank i bitmap)     right (goR range)
-
--- The position of the i-th occurence of a given symbol in the sequence
-wvselect :: (Wv a, Integral ix) => a -> ix -> WaveletTree a -> ix
-wvselect c i (WaveletTree w range) = wvselect' c (i-1) w range
-  where wvselect' _ i (Leaf _) _ = i -- all unused arguments should contain the same symbol
-        wvselect' c i (Node _ left Dummy) range  = wvselect' c i left (goL range)
-        wvselect' c i (Node _ Dummy right) range = wvselect' c i right (goR range)
-        wvselect' c i (Node bitmap left right) range@(a,b)
-          | c <= (midpoint a b) = let j = wvselect' c i left (goL range)  in select False (j+1) bitmap
-          | otherwise           = let j = wvselect' c i right (goR range) in select True  (j+1) bitmap
+  select c i (WaveletTree w range) = select' c (i-1) w range
+    where select' _ i (Leaf _) _ = i -- all unused arguments should contain the same symbol
+          select' c i (Node _ left Dummy) range  = select' c i left (goL range)
+          select' c i (Node _ Dummy right) range = select' c i right (goR range)
+          select' c i (Node bitmap left right) range@(a,b)
+            | c <= (midpoint a b) = let j = select' c i left (goL range)  in select False (j+1) bitmap
+            | otherwise           = let j = select' c i right (goR range) in select True  (j+1) bitmap
 
 test :: IO ()
 test = do
@@ -94,5 +110,5 @@ test = do
       (n,w) = (length str, wavelet str)
       str'  = (w!)<$>[0..n-1]
   print str'; print $ str==str' -- this check should always hold (!)
-  mapM_ print (map (\c -> map (\i->wvrank c i w) [1..n]) (sort.nub$str))
-  print $ let c = str!!0 in map (\i->wvselect c i w) [1..length$filter(==c)str]
+  mapM_ print (map (\c -> map (\i->rank c i w) [1..n]) (sort.nub$str))
+  print $ let c = str!!0 in map (\i->select c i w) [1..length$filter(==c)str]
