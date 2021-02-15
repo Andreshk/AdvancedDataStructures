76733785861092561b6d7f6b4a73f1d64051f9e5
Andrey Drenski <andreshko.dre@gmail.com>
2021-02-15 18:29:28 +0200

    add pretty-printing & generalize to any ordered type

--- CartesianTree.hs
+++ CartesianTree.hs
@@ -1,10 +1,30 @@
 module CartesianTree where
 
-data CartesianTree = Empty | Node Int CartesianTree CartesianTree
-  deriving (Eq, Show) -- both used for debugging, neither required for normal work
+data CartesianTree a = Empty | Node a (CartesianTree a) (CartesianTree a)
+
+-- Used purely for debugging purposes
+instance Show a => Show (CartesianTree a) where
+    show t = let (res, _, _) = show' t in init $ unlines res
+      where -- Returns the lines of the visualized tree + dimension of the rectangle it fits in.
+            -- See below for its postconditions (checked at every return).
+            show' :: Show a => CartesianTree a -> ([String], Int, Int)
+            show' Empty = checkPost ([],0,0)
+            show' (Node val left right) = checkPost (first:rest, lw+vw+rw, rows+1)
+              where pad :: Int -> String
+                    pad n = replicate n ' '
+                    first = (pad lw) ++ show val ++ (pad rw)
+                    vw = length $ show val
+                    (lres, lw, lh) = show' left
+                    (rres, rw, rh) = show' right
+                    rows = max lh rh
+                    rest = zipWith (\l r -> l ++ (pad vw) ++ r) (lres ++ replicate (rows - lh) (pad lw))
+                                                                (rres ++ replicate (rows - rh) (pad rw))
+            -- Checks the show' postconditions (impurely). Can be overridden to id.
+            checkPost (res,w,h) = if ok then (res,w,h) else error "show' postcondition violated"
+              where ok = length res == h && all ((==w).length) res
 
 -- Omega(nlgn), but O(n^2) if the tree ends up imbalanced (no control over that)
-cartesian :: [Int] -> CartesianTree
+cartesian :: Ord a => [a] -> CartesianTree a
 cartesian []  = Empty
 cartesian [x] = Node x Empty Empty -- unneeded, really
 cartesian lst = Node m (cartesian lst1) (cartesian lst2)
@@ -14,16 +34,16 @@ cartesian lst = Node m (cartesian lst1) (cartesian lst2)
 -- Also Omega(nlgn) and O(n^2), but expected to be faster in practice.
 -- The (<<) operator inserts a value down the right spine of a tree,
 -- therefore building the tree is calling (<<) for each value in the list.
-cartesian' :: [Int] -> CartesianTree
+cartesian' :: Ord a => [a] -> CartesianTree a
 cartesian' = foldl (<<) Empty
-  where (<<) :: CartesianTree -> Int -> CartesianTree
+  where (<<) :: Ord a => CartesianTree a -> a -> CartesianTree a
         Empty                    << x = Node x Empty Empty
         tr@(Node val left right) << x
           | x < val   = Node x tr Empty
           | otherwise = Node val left (right << x)
 
 -- Reduction from LCA to 0-1 RMQ (pardon the ugly function name)
-lca2rmq01 :: CartesianTree -> [Int]
+lca2rmq01 :: CartesianTree a -> [Int]
 lca2rmq01 Empty = []
 lca2rmq01 tr    = helper tr 0
   where helper (Node _ left right) d = [d] ++ try left d ++ try right d
