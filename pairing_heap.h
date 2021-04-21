c91b4f405228e79b2074e37c4a3980cbdfd76ccd
Andreshk <andreshko.dre@gmail.com>
2021-04-21 12:12:37 +0300

    major improvements: - do not use a vector during extractMin() - proper recursive & iterative pairwise merging is now implemented - merge() now takes rvalues only to make it obvious the argument will be empty afterwards - move-assignment is now noexcept, as it should've been - removed the unnecessary swap() methods - improved comments & added vasserts

--- pairing_heap.h
+++ pairing_heap.h
@@ -1,41 +1,45 @@
 #pragma once
-#include <vector>
 #include <cstddef>    // size_t
 #include <algorithm>  // std::swap
 #include <functional> // std::less
+#include <utility>    // std::exchange
+#include "vassert.h"
 
 template<class T, class Compare = std::less<T>>
 class PairingHeap {
-    struct node {
+    struct Node {
         T value;
-        node* leftChild;
-        node* rightSibling;
-        node* predecessor; // parent or left sibling
-        node(const T& val, node* pred = nullptr) 
+        Node* leftChild;
+        Node* rightSibling;
+        Node* predecessor; // parent or left sibling
+        Node(const T& val, Node* pred = nullptr) 
             : value{ val }, leftChild{ nullptr }, rightSibling{ nullptr }, predecessor{ pred } {}
     };
 
     // Invariant: root->predecessor == root->rightSibling == nullptr
-    node* root;
+    Node* root;
     size_t count;
     Compare cmp;
+    // Controls whether to use a recursive or iterative approach for the
+    // pairwise merge, specific to this heap's extractMin procedure.
+    static constexpr bool useRecursiveMerge = false;
 
     // Helper copying/deleting functions
-    static node* copyNode(const node*, node*);
-    static void freeNode(const node*);
-    static node* mergeNode(node*, node*, Compare&);
+    static Node* copyNode(const Node*, Node*);
+    static void freeNode(const Node*);
+    // Merge two trees
+    static Node* mergeNode(Node*, Node*, Compare&);
+    // Recursive pairwise merge of a list of siblings (controlled by useRecursiveMerge)
+    static Node* pairwiseMerge(Node*, Compare&);
     void copyFrom(const PairingHeap&);
-
-    // Constructing a heap form a root pointer (received, perhaps, from a child node)
-    // without knowing the heap's size. Used for making a singleton heap.
-    explicit PairingHeap(node* root, size_t count = 0, const Compare& cmp = Compare{})
-        : root{ root }, count{ count }, cmp{ cmp } {}
 public:
+    // Represents an "iterator" or key, returned at insertion,
+    // which can later be used to decrease the inserted value.
     class proxy {
-        friend class PairingHeap<T, Compare>;
-        node* ptr;
+        friend class PairingHeap<T, Compare>; // Only a heap can construct proxies to itself
+        Node* ptr;
 
-        explicit proxy(node* ptr) : ptr{ ptr } {}
+        explicit proxy(Node* ptr) : ptr{ ptr } {}
     public:
         proxy() : ptr{ nullptr } {}
         const T& operator*()  const { return  ptr->value; }
@@ -46,18 +50,17 @@ public:
     };
 
     // Standard big 6.
-    // Move ctor _must_ be noexcept is order for std::vector<PairingHeap>::push_back()
-    // to work properly (move heaps, not copy them) during extractMin().
-    PairingHeap(const Compare& cmp = Compare{}) noexcept : PairingHeap{ nullptr, 0, cmp } {}
+    explicit PairingHeap(const Compare& cmp = Compare{}) noexcept
+        : root{ nullptr }, count{ 0 }, cmp{ cmp } {}
     PairingHeap(const PairingHeap& other) : PairingHeap{} { copyFrom(other); }
-    PairingHeap(PairingHeap&& other) noexcept : PairingHeap{} { swap(other); }
+    PairingHeap(PairingHeap&& other) noexcept : PairingHeap{} { std::swap(*this, other); }
     PairingHeap& operator=(const PairingHeap&);
-    PairingHeap& operator=(PairingHeap&&);
+    PairingHeap& operator=(PairingHeap&&) noexcept;
     ~PairingHeap() { clear(); }
 
-    // x.merge(y) always leaves y empty (!) regardless which root value
-    // is smaller. Afterwards x contains all values of both x and y.
-    void merge(PairingHeap&);
+    // Merge the contents of a heap into this one & leaves it empty.
+    // API makes it clear that after calling x.merge(std::move(y)) y will be moved-from, i.e. empty.
+    void merge(PairingHeap&&);
 
     // Inserting a value - creates a singleton heap and merges with the current heap
     proxy insert(const T&);
@@ -68,31 +71,29 @@ public:
     // The most complex operation: removing the root and merging all of its children
     T extractMin();
 
-    // Special (!)
+    // Attempts decreasing the value, pointed to by a given proxy, with a new one.
+    // Returns whether this was successful (i.e. the provided value was smaller).
     bool decreaseKey(proxy, const T&);
 
     // More standard methods
     size_t size() const { return count; }
     bool empty() const { return (root == nullptr); }
     void clear();
-
-    // For convenience
-    void swap(PairingHeap&) noexcept;
 };
 
 template<class T, class Compare>
-auto PairingHeap<T, Compare>::copyNode(const node* ptr, node* pred) -> node* {
+auto PairingHeap<T, Compare>::copyNode(const Node* ptr, Node* pred) -> Node* {
     if (!ptr) {
         return nullptr;
     }
-    node* tmp = new node(ptr->value, pred);
+    Node* tmp = new Node(ptr->value, pred);
     tmp->leftChild = copyNode(ptr->leftChild, tmp);
     tmp->rightSibling = copyNode(ptr->rightSibling, tmp);
     return tmp;
 }
 
 template<class T, class Compare>
-void PairingHeap<T, Compare>::freeNode(const node* ptr) {
+void PairingHeap<T, Compare>::freeNode(const Node* ptr) {
     if (ptr) {
         freeNode(ptr->leftChild);
         freeNode(ptr->rightSibling);
@@ -101,7 +102,9 @@ void PairingHeap<T, Compare>::freeNode(const node* ptr) {
 }
 
 template<class T, class Compare>
-auto PairingHeap<T, Compare>::mergeNode(node* root1, node* root2, Compare& cmp) -> node* {
+auto PairingHeap<T, Compare>::mergeNode(Node* root1, Node* root2, Compare& cmp) -> Node* {
+    vassert(!root1->rightSibling && !root1->predecessor);
+    vassert(!root2->rightSibling && !root2->predecessor);
     // For simplicity, let root1 be the heap that "adopts" the other
     if (cmp(root2->value, root1->value)) {
         std::swap(root1, root2);
@@ -117,6 +120,28 @@ auto PairingHeap<T, Compare>::mergeNode(node* root1, node* root2, Compare& cmp) 
     root2->predecessor = root1;
     // Return the root of the merged heap
     return root1;
+}
+
+template <class T, class Compare>
+auto PairingHeap<T, Compare>::pairwiseMerge(Node* first, Compare& cmp) -> Node* {
+    vassert(!(first && first->predecessor));
+    if (!first || !first->rightSibling) {
+        return first;
+    }
+    // Isolate the first two siblings properly from the remaining list
+    Node* second = std::exchange(first->rightSibling, nullptr);
+    second->predecessor = nullptr;
+    Node* rest = std::exchange(second->rightSibling, nullptr);
+    if (rest) { rest->predecessor = nullptr; }
+    // Recursively merge the rest, if any
+    rest = pairwiseMerge(rest, cmp);
+    Node* firstTwo = mergeNode(first, second, cmp);
+    // Combine the merged first two siblings w/ the merged remaining
+    if (rest) {
+        return mergeNode(firstTwo, rest, cmp);
+    } else {
+        return firstTwo;
+    }
 }
 
 template<class T, class Compare>
@@ -136,21 +161,21 @@ auto PairingHeap<T, Compare>::operator=(const PairingHeap& other) -> PairingHeap
 }
 
 template<class T, class Compare>
-auto PairingHeap<T, Compare>::operator=(PairingHeap&& other) -> PairingHeap& {
+auto PairingHeap<T, Compare>::operator=(PairingHeap&& other) noexcept -> PairingHeap& {
     if (this != &other) {
         clear();
-        swap(other);
+        std::swap(*this, other);
     }
     return *this;
 }
 
 template<class T, class Compare>
-void PairingHeap<T, Compare>::merge(PairingHeap& other) {
+void PairingHeap<T, Compare>::merge(PairingHeap&& other) {
     // Some edge cases are handled way earlier
     if (this == &other || other.empty()) {
         // nothing to do
     } else if (empty()) {
-        swap(other);
+        std::swap(*this, other);
     } else {
         root = mergeNode(root, other.root, cmp);
         count += other.count;
@@ -160,9 +185,9 @@ void PairingHeap<T, Compare>::merge(PairingHeap& other) {
 }
 
 template<class T, class Compare>
-auto PairingHeap<T, Compare>::insert(const T& _val) -> proxy {
+auto PairingHeap<T, Compare>::insert(const T& value) -> proxy {
     // Simple: make a new heap and merge it
-    node* res = new node(_val);
+    Node* res = new Node(value);
     root = (count == 0 ? res : mergeNode(root, res, cmp));
     ++count;
     return proxy{ res };
@@ -170,33 +195,59 @@ auto PairingHeap<T, Compare>::insert(const T& _val) -> proxy {
 
 template<class T, class Compare>
 T PairingHeap<T, Compare>::extractMin() {
-    // Saving the root's value & leftChild before freeing the node
+    // Save the root's value & leftChild before freeing the node
     const T result = peek();
-    node* nextChild = root->leftChild;
+    Node* first = root->leftChild;
     delete root;
     root = nullptr;
-    // The old root's children (also heaps)
-    std::vector<node*> children;
-    while (nextChild) {
-        node* curr = nextChild;
-        nextChild = nextChild->rightSibling;
-        curr->rightSibling = curr->predecessor = nullptr;
-        children.push_back(curr);
+    // If no children, nothing else to do
+    if (first) {
+        first->predecessor = nullptr;
+        // Merge children in pairs & the pairs back-to-front, recursively or iteratively
+        if constexpr (useRecursiveMerge) {
+            root = pairwiseMerge(first, cmp);
+        } else {
+            // First build the list of merged pairs, keeping a pointer to its rightmost member
+            Node* last = nullptr;
+            while (first) {
+                vassert(!first->predecessor && !(last && last->rightSibling));
+                if (!first->rightSibling) { // No sibling to pair it with, nothing more to do
+                    first->predecessor = last;
+                    last = first;
+                    break;
+                }
+                // Isolate the first two siblings properly (some of these assignments might be unnecessary, yes)
+                Node* second = std::exchange(first->rightSibling, nullptr);
+                second->predecessor = nullptr;
+                Node* rest = std::exchange(second->rightSibling, nullptr);
+                if (rest) { rest->predecessor = nullptr; }
+                // Merge this pair & "insert" it to the back of the merged pairs list
+                Node* firstTwo = mergeNode(first, second, cmp);
+                firstTwo->predecessor = last;
+                last = firstTwo;
+                // Continue to the next sibling to be merged
+                first = rest;
+            }
+            // Now that we have a pointer to the last pair, merge back into one
+            while (last->predecessor) {
+                vassert(!last->rightSibling);
+                // Again, temporarily isolate the two to be merged (just in case)
+                Node* prev = std::exchange(last->predecessor, nullptr);
+                prev->rightSibling = nullptr;
+                Node* rest = std::exchange(prev->predecessor, nullptr);
+                if (rest) { rest->rightSibling = nullptr; }
+                // Merge the two & reattach them to the back of the list
+                last = mergeNode(last, prev, cmp);
+                last->predecessor = rest;
+                if (rest) {
+                    rest->rightSibling = last;
+                }
+            }
+            // We now have one tree remaining
+            root = last;
+        }
     }
-    const size_t n = children.size();
-    // First merge the children in pairs - that's where the name comes from
-    if (n > 1) {
-        for (size_t i = 0; i <= (n - 2 - (n % 2)); i += 2)
-            children[i] = mergeNode(children[i], children[i + 1], cmp);
-    }
-    // Then merge the resulting heaps from the last to the first
-    if (n > 0) {
-        for (size_t i = (n - 2 + (n % 2)); i > 0; i -= 2)
-            children[i - 2] = mergeNode(children[i - 2], children[i], cmp);
-        // The only heap left in the array is the one 
-        root = children[0];
-    }
-    // Decrease the heap size (lost in the final swap)
+    vassert(!(root && (root->predecessor || root->rightSibling)));
     --count;
     return result;
 }
@@ -204,12 +255,11 @@ T PairingHeap<T, Compare>::extractMin() {
 template<class T, class Compare>
 bool PairingHeap<T, Compare>::decreaseKey(proxy pr, const T& newKey) {
     // If the proxy does not point to a node in this heap -> undefined behaviour(!)
-    // In case of invalid input
     if (!cmp(newKey, *pr)) {
         return false;
     }
     // Update the value
-    node* location = pr.ptr;
+    Node* location = pr.ptr;
     location->value = newKey;
     // If the value is at the root (<=> no predecessor), nothing more to change
     if (location == root) {
@@ -227,10 +277,7 @@ bool PairingHeap<T, Compare>::decreaseKey(proxy pr, const T& newKey) {
     }
     // Isolate the current node as a root of a new heap
     location->rightSibling = location->predecessor = nullptr;
-    // Keep the old size
-    const size_t oldCount = count;
-    // Merge back the singleton heap, represented by
-    // the isolated node with the decreased value.
+    // Merge this heap back into the remainder
     root = mergeNode(root, location, cmp);
     return true;
 }
@@ -238,18 +285,6 @@ bool PairingHeap<T, Compare>::decreaseKey(proxy pr, const T& newKey) {
 template<class T, class Compare>
 void PairingHeap<T, Compare>::clear() {
     freeNode(root);
-    root = nullptr;
+    root  = nullptr;
     count = 0;
-}
-
-template<class T, class Compare>
-void PairingHeap<T, Compare>::swap(PairingHeap& other) noexcept {
-    std::swap(root, other.root);
-    std::swap(count, other.count);
-    std::swap(cmp, other.cmp);
-}
-
-template<class T, class Compare>
-void swap(PairingHeap<T, Compare>& lhs, PairingHeap<T, Compare>& rhs) {
-    lhs.swap(rhs);
 }
