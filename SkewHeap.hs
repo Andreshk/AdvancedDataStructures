b9763cc4c87537d2cf12dffd8055f9399a6a72c0
Andrey Drenski <andreshko.dre@gmail.com>
2022-04-14 21:18:17 +0300

    minor refactor & better comments

--- SkewHeap.hs
+++ SkewHeap.hs
@@ -11,40 +11,36 @@ import Data.List (unfoldr)
 -- amortized complexity. toList, fromList and sort are obviously O(nlgn).
 data SkewHeap a = Empty | Node a (SkewHeap a) (SkewHeap a)
 
-instance (Ord a) => Eq (SkewHeap a) where
-    sh1 == sh2 = (toList sh1) == (toList sh2)
+-- The characteristic operation - a skew merge
+(<>) :: Ord a => SkewHeap a -> SkewHeap a -> SkewHeap a
+sh1 <> Empty = sh1
+Empty <> sh2 = sh2
+sh1@(Node v1 l1 r1) <> sh2@(Node v2 l2 r2)
+    | v1 < v2   = Node v1 (sh2 <> r1) l1
+    | otherwise = Node v2 (sh1 <> r2) l2
 
-instance (Show a) => Show (SkewHeap a) where
-    show Empty = "SkewHeap{}"
-    show (Node val _ _) = "SkewHeap{" ++ show val ++ "..}"
-
+-- All other operations are then reduced to a skew merge
 insert :: Ord a => a -> SkewHeap a -> SkewHeap a
 insert x sh = singleton x <> sh
   where singleton x = Node x Empty Empty
 
-(<>) :: Ord a => SkewHeap a -> SkewHeap a -> SkewHeap a
-(<>) = merge
-
 merge :: Ord a => SkewHeap a -> SkewHeap a -> SkewHeap a
-merge sh1 Empty = sh1
-merge Empty sh2 = sh2
-merge sh1@(Node v1 l1 r1) sh2@(Node v2 l2 r2)
-    | v1 < v2   = Node v1 (sh2 <> r1) l1
-    | otherwise = Node v2 (sh1 <> r2) l2
-
-getMin :: SkewHeap a -> Maybe a
-getMin Empty = Nothing
-getMin (Node val _ _) = Just val
+merge = (<>)
 
 extractMin :: Ord a => SkewHeap a -> Maybe (a, SkewHeap a)
 extractMin Empty = Nothing
 extractMin (Node val left right) = Just (val, left <> right)
 
+-- Other basic operations
+getMin :: SkewHeap a -> Maybe a
+getMin Empty = Nothing
+getMin (Node val _ _) = Just val
+
 empty :: SkewHeap a -> Bool
 empty Empty = True
 empty _ = False
 
--- unfortunately O(n), as is length for lists
+-- Unfortunately O(n)
 size :: Ord a => SkewHeap a -> Int
 size = length . toList
 
