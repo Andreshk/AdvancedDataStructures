8cba1d5b66555dc3f3fb7a6250e2bd7e78853a3c
Andrey Drenski <andrey.drenski@chaosgroup.com>
2022-07-22 13:52:44 +0300

    implement & use some missing C++20 features for improved safety

--- binomial_heap_meta.cxx
+++ binomial_heap_meta.cxx
@@ -1,6 +1,24 @@
 #include <iostream> // for visualization purposes only
 #include <algorithm> // std::min
 #include <array>
+#include <cassert>
+
+// Circle uses libstdc++-10.3, where some C++20 libraries are still missing
+// Barebones replacement for std::span<const int, N>
+template <unsigned N>
+struct Span {
+    const int* ptr;
+    Span(const int* ptr) : ptr{ptr} {}
+    Span(const std::array<int, N>& data) : ptr{ &data[0] } {}
+    template <unsigned Off, unsigned N1 = N - Off> requires (Off + N1 <= N)
+    Span<N1> sub() const { return { ptr + Off }; }
+    const int& operator[](size_t idx) const { assert(idx < N); return ptr[idx]; }
+    static constexpr unsigned size() { return N; }
+};
+// More replacements: some <bit> functions - thank god GCC's builtins are constexpr
+constexpr bool PowerOf2(unsigned n) { return (n > 0 && !(n&(n-1))); }
+constexpr int Log2(unsigned n) { return __builtin_ctz(n); } // works for powers of 2 only!
+static_assert(Log2(8) == 3 && PowerOf2(16));
 
 template <unsigned Rank>
 struct BinomialTree {
@@ -22,34 +40,31 @@ BinomialTree<Rank+1> merge(const BinomialTree<Rank>& left, const BinomialTree<Ra
     }
 }
 
-// This should take span<int, N> - why does Circle not have std::span???
-template <unsigned Rank>
-BinomialTree<Rank> makeTree(const int* values) {
-    static constexpr unsigned N = (1u<<Rank);
+template <unsigned N> requires (PowerOf2(N))
+BinomialTree<Log2(N)> makeTree(Span<N> values) {
+    static constexpr unsigned Rank = Log2(N);
     if constexpr (Rank == 0) {
         return { values[0] };
     } else {
-        return merge(makeTree<Rank-1>(values),
-                     makeTree<Rank-1>(&values[N/2]));
+        return merge(makeTree(values.template sub<0,N/2>()),
+                     makeTree(values.template sub<N/2>()));
     }
 }
 
 template <unsigned N>
 class BinomialHeap {
-    // Holder for the trees' ranks
+    // Holders for the trees' ranks and sizes
     static constexpr std::array Ranks = { int...(32).filter(N&(1u<<_0))... };
+    static constexpr std::array Sizes = { (1u<<Ranks.[:])... };
     [[no_unique_address]] BinomialTree<Ranks.[:]> ...ts;
 public:
     // Splits the input array into chunks the size of each tree
     // and directly initializes the subtrees from these chunks.
-    BinomialHeap(const std::array<int, N>& values)
-        : ts{ makeTree<Ranks.[:]>(&values[N&((1u<<Ranks.[:])-1)]) }... {}
+    BinomialHeap(Span<N> values)
+        : ts{ makeTree(values.template sub<N % Sizes.[:], Sizes.[:]>()) }... {}
     int getMin() const { return (... std::min ts.value); } // lol
     bool valid() const { return (... && ts.valid()); }
 };
-// Helper deduction guide
-template <unsigned N>
-BinomialHeap(const std::array<int,N>&) -> BinomialHeap<N>;
 
 // Stream output, nothing special
 template <unsigned Rank>
@@ -65,8 +80,8 @@ std::ostream& operator<<(std::ostream& os, const BinomialHeap<N>& h) {
 }
 
 int main() {
-    int values[] = { 2,4,3,8,1,7,6,5 };
-    const auto t = makeTree<3>(values);
+    const Span values = std::array{ 2,4,3,8,1,7,6,5 };
+    const auto t = makeTree(values);
     constexpr unsigned R = decltype(t)::rank();
     std::cout << "Ranks of BinomialTree<" << R << ">'s subtrees:";
     std::cout << ' ' << t.ts.rank() ...;
@@ -74,12 +89,12 @@ int main() {
     std::cout << "BinomialTree<" << R << ">: " << t << " (valid: " << std::boolalpha << t.valid() << ")\n";
     static_assert(sizeof(t) == (sizeof(t.value) << R)); // A tree of rank R has 2^R values.
     
-    constexpr unsigned N = 13;
-    std::array<int, N> values1 = { 5,13,9,1,7,11,6,3,4,10,2,12,8 };
+    const Span values1 = std::array{ 5,13,9,1,7,11,6,3,4,10,2,12,8 };
+    constexpr unsigned N = values1.size();
     const BinomialHeap h{ values1 };
     std::cout << "Ranks of BinomialHeap<" << N << ">'s trees:";
-    std::cout << ' ' << h.ts.rank() ...;
-    std::cout <<'\n';
+    for (int R : h.Ranks) { std::cout << ' ' << R; }
+    std::cout << '\n';
     std::cout << "BinomialHeap<" << N << ">: " << h
               << " (min=" << h.getMin() << "; valid: " << h.valid() << ")\n";
     static_assert(sizeof(h) == sizeof(t.value) * N);
